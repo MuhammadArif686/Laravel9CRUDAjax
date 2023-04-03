9b4fca5e4eb1a8eea82abf79ba912bbbac1e368c
Muhamad Arif <muhamad.arif@bumn.go.id>
2023-04-03 13:52:04 +0700

    fix input data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -12,5 +13,28 @@ class PostController extends Controller
         $posts = Post::latest()->get();
 
         return view('posts', compact('posts'));
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Berhasil disimpan',
+            'data' => $post
+        ]);
     }
 }
