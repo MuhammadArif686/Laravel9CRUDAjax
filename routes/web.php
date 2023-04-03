4cb454d0b9ef7646c217f2302ae78232ede52970
Muhamad Arif <muhamad.arif@bumn.go.id>
2023-04-03 10:29:17 +0700

    fix db, show data from database

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('posts');
+// });
+
+Route::resource('/', App\Http\Controllers\PostController::class);
+Route::resource('/posts', App\Http\Controllers\PostController::class);
